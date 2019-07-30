b3fca87216e19c43dd8b56297a82ef8c2d6a2aa1
Rosty Pash <>
2019-07-31 00:26:10 +0300

    init commit

---
+++ src/main/java/com/symphonysolutions/CarPoolApplication.java
@@ -0,0 +1,13 @@
+package com.symphonysolutions;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CarPoolApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CarPoolApplication.class, args);
+	}
+
+}
